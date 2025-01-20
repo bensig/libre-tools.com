065144c5a4db36689e1217090d4bcaed8e8c02ba
bensig <bensig@gmail.com>
2025-01-20 14:17:29 -0800

    update colors and fonts

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,25 +4,24 @@ import { Navbar, Container, Nav } from 'react-bootstrap';
 const Layout = ({ children }) => {
   return (
     <>
-      <Navbar bg="dark" variant="dark" expand="lg" className="mb-4">
+      <Navbar bg="primary" variant="dark" expand="lg" className="mb-4">
         <Container fluid>
           <Navbar.Brand as={Link} to="/">
             <img
-              src="/libre-favicon.png"
-              width="30"
+              src="/Libre Favicon White.png"
               height="30"
               className="d-inline-block align-top me-2"
               alt="Libre logo"
             />
-            Libre Tools
+          Libre Tools
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link as={Link} to="/">Home</Nav.Link>
-              <Nav.Link as={Link} to="/explorer">Smart Contract Explorer</Nav.Link>
-              <Nav.Link as={Link} to="/transactions">Transaction History</Nav.Link>
-              <Nav.Link as={Link} to="/btc-tracker">BTC Tracker</Nav.Link>
+              <Nav.Link as={Link} to="/" className="fs-6">Home</Nav.Link>
+              <Nav.Link as={Link} to="/explorer" className="fs-6">Smart Contract Explorer</Nav.Link>
+              <Nav.Link as={Link} to="/transactions" className="fs-6">Transaction History</Nav.Link>
+              <Nav.Link as={Link} to="/btc-tracker" className="fs-6">BTC Tracker</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
