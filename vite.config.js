7962571c7bf0c68b13293799f2cbfb39cb4191e1
bensig <bensig@gmail.com>
2025-02-04 23:24:34 -0800

    unbundle

--- vite.config.js
+++ vite.config.js
@@ -4,36 +4,4 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.png'],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: (id) => {
-          // Vendor chunks
-          if (id.includes('node_modules')) {
-            if (id.includes('react') || 
-                id.includes('scheduler') || 
-                id.includes('@remix-run') ||
-                id.includes('react-dom')) {
-              return 'react-vendor'
-            }
-            if (id.includes('bootstrap') || 
-                id.includes('@popperjs')) {
-              return 'bootstrap-vendor'
-            }
-            return 'vendor'
-          }
-          // App specific chunks
-          if (id.includes('word-list')) {
-            return 'wordlist'
-          }
-          if (id.includes('SeedGenerator')) {
-            return 'seed-generator'
-          }
-        }
-      }
-    },
-    chunkSizeWarningLimit: 500,
-    minify: 'esbuild',
-    target: 'esnext'
-  }
 });
